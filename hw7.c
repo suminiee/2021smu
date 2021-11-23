8b20c194bbfc4cdf94ca0488273bb0c7281d6f2d
suminiee <90146604+suminiee@users.noreply.github.com>
2021-11-23 22:56:25 +0900

    hw7.c

--- hw7.c
+++ hw7.c
@@ -1,1 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include <math.h>
+
+double func(double num[], int size, double* mean, double* stdev)
+{
+	double sum = 0;
+	double ssum = 0;
+	double variance;//분산
+	int i;
+
+	for (i = 0; i < size; i++) //전체 합
+	{
+		sum = sum + num[i];
+	}
+	*mean = sum / size; //평균
+	for (i = 0; i < size; i++)
+	{
+		ssum += (num[i] - *mean) * (num[i] - *mean);
+	}
+	variance = ssum / size; //분산
+	*stdev = sqrt(variance); // 표준편차
+}
+
+int main(void)
+{
+	double num[5];
+	int i;
+	double mean, p_mean, p_stdev;
+
+	printf("Enter 5 real numbers: ");
+	for (i = 0; i < 5; i++)
+	{
+		scanf("%lf", &num[i]);
+	}
+	func(num, 5, &p_mean, &p_stdev);
+	printf("Standard Deviation = %.3lf", p_stdev);
+	return 0;
+
+}
